dbf6c222ca6363fcb07a49c539e3374a7d856fac
blaec <odeskonst@yandex.ru>
2018-05-21 21:27:18 +0300

    adding repaint frame

---
+++ src/main/java/org/blaec/HeadFirstJava/MyDrawPanel.java
@@ -0,0 +1,25 @@
+package org.blaec.HeadFirstJava;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyDrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setPaint(new GradientPaint(70, 70, getRandomColor(), 150, 150, getRandomColor()));
+        g.fillOval(70, 70, 100, 100);
+    }
+
+    private int getRandomHue() {
+        return (int) (Math.random() * 255);
+    }
+
+    private Color getRandomColor() {
+        int red = getRandomHue();
+        int green = getRandomHue();
+        int blue = getRandomHue();
+
+        return new Color(red, green, blue);
+    }
+}

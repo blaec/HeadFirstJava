3c99041b705c492bf84eae77158605d1e89bad12
blaec <odeskonst@yandex.ru>
2018-05-21 21:45:39 +0300

    two action listeners

---
+++ src/main/java/org/blaec/HeadFirstJava/TwoButton.java
@@ -0,0 +1,50 @@
+package org.blaec.HeadFirstJava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TwoButton {
+    JFrame frame;
+    JLabel label;
+
+    public static void main(String[] args) {
+        TwoButton gui = new TwoButton();
+        gui.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton labelButton = new JButton("Change label");
+        labelButton.addActionListener(new LabelListener());
+
+        JButton colorButton = new JButton("Change Circle");
+        colorButton.addActionListener(new ColorListener());
+
+        label = new JLabel("I'm a label");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+
+    private class LabelListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            label.setText("Ouch!");
+        }
+    }
+
+    private class ColorListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            frame.repaint();
+        }
+    }
+}

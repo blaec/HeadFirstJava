c076e574bbae17e19535decdab1392b8431978fc
blaec <odeskonst@yandex.ru>
2018-05-21 22:54:54 +0300

    music player

---
+++ src/main/java/org/blaec/HeadFirstJava/MiniMusicPlayer.java
@@ -0,0 +1,28 @@
+package org.blaec.HeadFirstJava;
+
+import javax.sound.midi.*;
+
+import static org.blaec.HeadFirstJava.MidiUtil.makeEvent;
+
+public class MiniMusicPlayer {
+    public static void main(String[] args) {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            for (int i = 5; i < 61; i += 4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(128, 1, i, 100, i + 2));
+            }
+
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

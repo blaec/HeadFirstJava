c0e97dfe08e0ca1e02c7109d6af97fdbe56e839c
blaec <odeskonst@yandex.ru>
2018-05-23 22:13:29 +0300

    saving quiz cards

---
+++ src/main/java/org/blaec/HeadFirstJava/saving/quizcard/QuizCard.java
@@ -0,0 +1,22 @@
+package org.blaec.HeadFirstJava.saving.quizcard;
+
+import javax.swing.*;
+
+public class QuizCard {
+    private String question;
+    private String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+}
